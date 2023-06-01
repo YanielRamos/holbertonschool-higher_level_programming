d1626b0bbe06977287475398973be0f5f07e0133
YanielRamos <yanielramos0504@gmail.com>
2023-06-01 14:05:18 -0400

    fixing 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,8 @@
 
 for dgt1 in range(10):
     for dgt2 in range(dgt1 + 1, 10):
-        print("{:d}{:d}".format(dgt1, dgt2), end=", ")
+        if dgt1 == 8 and dgt2 == 9:
+            print("{:d}{:d}".format(dgt1, dgt2), end="")
+        else:
+            print("{:d}{:d}".format(dgt1, dgt2), end=", ")
 print()
