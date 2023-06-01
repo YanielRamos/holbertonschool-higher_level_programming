76d0d2540c4fbb2e3cbe917b09c546c95799d3f7
YanielRamos <yanielramos0504@gmail.com>
2023-06-01 11:09:58 -0400

    adding file 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -3,7 +3,5 @@
 for i in range(0, 100):
     if i == 99:
         print(i, end="\n")
-    elif i <= 9:
-        print("0{}".format(i), end=", ")
-    else:
-        print("{}".format(i), end=", ")
+    elif i < 99:
+        print("{:02d}".format(i), end=", ")
