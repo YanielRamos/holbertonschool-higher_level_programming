1911259cb1de4bd0682d65a32ab61ba0f4ce0d0d
YanielRamos <yanielramos0504@gmail.com>
2023-06-02 16:54:03 -0400

    adding file 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+
+    args = sys.argv[1:]
+    args_counter = len(args)
+
+    if args_counter == 0:
+        print("{} arguments.".format(args_counter))
+    elif args_counter == 1:
+        print("{} argument:".format(args_counter), end="\n")
+        print(f"{args_counter}: {args}", end="\n")
+    else:
+        print(f"{args_counter} arguments:", end="\n")
+        for i, arg in enumerate(args, start=1):
+            print(f"{i}: {arg}", end="\n")
