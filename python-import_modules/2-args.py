2f343809fd573c265709fc432a1a7917ba9d7f46
YanielRamos <yanielramos0504@gmail.com>
2023-06-02 16:58:54 -0400

    fixing 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         print("{} arguments.".format(args_counter))
     elif args_counter == 1:
         print("{} argument:".format(args_counter), end="\n")
-        print(f"{args_counter}: {args}", end="\n")
+        print(f"{args_counter}:", "".join(args), end="\n")
     else:
         print(f"{args_counter} arguments:", end="\n")
         for i, arg in enumerate(args, start=1):
