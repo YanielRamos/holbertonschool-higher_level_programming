95de4d843caf06a14f21bb98f53246fd14e0796f
YanielRamos <yanielramos0504@gmail.com>
2023-06-02 16:12:46 -0400

    fixing 1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
 
-a = 10
-b = 5
+if __name__ == "__main__":
+    a = 10
+    b = 5
 
-add_result = add(a, b)
-print("{} + {} = {}".format(a, b, add_result), end="\n")
+    add_result = add(a, b)
+    print("{} + {} = {}".format(a, b, add_result), end="\n")
 
-sub_result = sub(a, b)
-print("{} - {} = {}".format(a, b, sub_result), end="\n")
+    sub_result = sub(a, b)
+    print("{} - {} = {}".format(a, b, sub_result), end="\n")
 
-mul_result = mul(a, b)
-print("{} * {} = {}".format(a, b, mul_result), end="\n")
+    mul_result = mul(a, b)
+    print("{} * {} = {}".format(a, b, mul_result), end="\n")
 
-div_result = div(a, b)
-print("{} / {} = {}".format(a, b, div_result), end="\n")
+    div_result = div(a, b)
+    print("{} / {} = {}".format(a, b, div_result), end="\n")
