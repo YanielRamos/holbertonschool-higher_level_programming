014f10de062a71b5003b5b51d155e1ba5a2a30d8
YanielRamos <yanielramos0504@gmail.com>
2023-06-05 22:57:08 -0400

    addibg file 6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+
+    if matrix is None:
+        return
+
+    for row in matrix:
+        for i, element in enumerate(row):
+            if i != len(row) - 1:
+                print(element, end=' ')
+            else:
+                print(element, end='$')
+        print()
