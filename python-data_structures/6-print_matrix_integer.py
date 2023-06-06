80aee1283260388df24bb7e36592177d60bf4165
YanielRamos <yanielramos0504@gmail.com>
2023-06-05 23:07:16 -0400

    file 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -6,25 +6,10 @@ def print_matrix_integer(matrix=[[]]):
     if matrix is None:
         return
 
-    '''for row in matrix:
+    for row in matrix:
         for i, digit in enumerate(row):
             if i != len(row) - 1:
                 print("{:d}".format(digit), end=' ')
             else:
                 print("{:d}$".format(digit), end='')
-        print()'''
-
-    if not matrix:
-        return
-
-    for row in matrix:
-        if not row:
-            return
-        else:
-            for i, element in enumerate(row):
-                if i != len(row) - 1:
-                    print("{:d} ".format(element), end='')
-                else:
-                    print("{:d}$".format(element), end='')
-            print()
-    print()
+        print()
