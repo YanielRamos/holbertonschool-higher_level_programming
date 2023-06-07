8c038965d51ee6937c8428c1b08b8d2a62fbac5f
YanielRamos <yanielramos0504@gmail.com>
2023-06-07 16:43:05 -0400

    fixing 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -8,8 +8,7 @@ def print_matrix_integer(matrix=[[]]):
 
     for row in matrix:
         for i, digit in enumerate(row):
-            if i != len(row) - 1:
-                print("{:d}".format(digit), end=' ')
-            else:
-                print("{:d}$".format(digit), end='')
-        print()
+            if i != 0:
+                print(" ", end="")
+            print("{:d}".format(digit), end='')
+        print("$")
