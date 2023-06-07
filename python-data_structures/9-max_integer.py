4418196059212bf76d96956859d22e90725565a5
YanielRamos <yanielramos0504@gmail.com>
2023-06-07 16:53:05 -0400

    fixing 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -6,6 +6,9 @@ def max_integer(my_list=[]):
     if my_list is None:
         return None
 
+    if len(my_list) == 0:
+        return None
+
     max_integer = sorted(my_list, reverse=True)
 
     return max_integer[0]
