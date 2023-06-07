6fbc328f487de9cdbd10a241c7b1a3ce451a62be
YanielRamos <yanielramos0504@gmail.com>
2023-06-06 22:57:08 -0400

    adding file 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -5,11 +5,13 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     if len(tuple_a) < 2:
         tuple_a += (0,) * (2 - len(tuple_a))
+
     elif len(tuple_a) > 2:
         tuple_a = tuple_a[:2]
 
     if len(tuple_b) < 2:
         tuple_b += (0,) * (2 - len(tuple_b))
+
     elif len(tuple_b) > 2:
         tuple_b = tuple_b[:2]
 
