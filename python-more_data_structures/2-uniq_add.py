ecb5a608a413de7f7eaeee728c80c89704e33e77
YanielRamos <yanielramos0504@gmail.com>
2023-06-07 23:28:58 -0400

    adding fille 2

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def uniq_add(my_list=[]):
+
+    if my_list is None or len(my_list) == 0:
+        return my_list
+
+    unique_int = set()
+    result = 0
+
+    for i in my_list:
+        if i not in unique_int:
+            unique_int.add(i)
+            result += i
+
+    return result
