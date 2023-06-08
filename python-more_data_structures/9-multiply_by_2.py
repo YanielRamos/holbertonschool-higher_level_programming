69980474db1dd0852b4c5c2c88b2fefec63c8080
YanielRamos <yanielramos0504@gmail.com>
2023-06-08 11:08:09 -0400

    adding file 9

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def multiply_by_2(a_dictionary):
+
+    new_dictionary = {}
+
+    for keys, value in a_dictionary.items():
+        new_dictionary[keys] = value * 2
+
+    return new_dictionary
