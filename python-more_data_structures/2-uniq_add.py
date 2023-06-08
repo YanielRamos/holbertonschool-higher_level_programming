8380e0b88d7792839ab3dc10f5e087b5736cc3e9
YanielRamos <yanielramos0504@gmail.com>
2023-06-07 23:45:36 -0400

    fixing 2

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -2,8 +2,9 @@
 
 
 def uniq_add(my_list=[]):
-    '''if my_list is None or len(my_list) == 0:
-        return my_list'''
+
+    if my_list is None or len(my_list) == 0:
+        return my_list
 
     uniq_list = set(my_list)
     num = 0
