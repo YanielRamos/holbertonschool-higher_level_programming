198214768c142aea7852ee04f2166ded5e45b3c1
YanielRamos <yanielramos0504@gmail.com>
2023-06-13 14:18:14 -0400

    fixing 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -25,7 +25,9 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
-        """printing the output"""
+        """
+        printing the output
+        """
         if self.__size == 0:
             print()
         else:
