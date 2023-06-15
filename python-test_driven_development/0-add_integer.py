b0532b0f4e3fe4ea7c6e37de951f93586f1de880
YanielRamos <yanielramos0504@gmail.com>
2023-06-14 22:59:02 -0400

    fixing file 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
+"""
+add_intger function
+"""
 
 
 def add_integer(a, b=98):
-    """Function that adds two variables"""
+    """
+    Adds two integers
+
+    Args:
+        a (int or float): The first number
+        b (int or float): The second number
+
+    Returns:
+        int: The addition of a and b
+
+    Raises:
+        TypeError: If a or b is not an integer or float
+    """
 
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
