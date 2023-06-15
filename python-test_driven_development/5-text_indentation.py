9c5f78b4228bd5816d25c8645ae069b5cb82336e
YanielRamos <yanielramos0504@gmail.com>
2023-06-15 18:57:55 -0400

    fixing 5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,15 +3,22 @@
 
 
 def text_indentation(text):
-    """function that indent text"""
+    """function that prints 2 new lines after [., ?, :] """
 
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
     symbols = [".", "?", ":"]
 
+    skip_space = False
+
     for char in text:
-        if char in symbols:
-            print(f"{char}\n", end="\n")
+        if skip_space:
+            if char != " ":
+                print(char, end="")
+                skip_space = False
+        elif char in symbols:
+            print(f"{char}\n", end="\n\n")
+            skip_space = True
         else:
             print(char, end="")
