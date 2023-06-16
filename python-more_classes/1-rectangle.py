c9abfdfa402c561a9f6a5c68cf520087908c9f97
YanielRamos <yanielramos0504@gmail.com>
2023-06-15 20:11:27 -0400

    adding file 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -7,8 +7,8 @@ class Rectangle:
     """Rectangle defines a rectangle"""
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -22,8 +22,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
@@ -37,5 +36,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
