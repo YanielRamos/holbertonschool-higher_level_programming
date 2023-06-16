6bef799c66ef0fcbb85858dfe24b0e2f0774f894
YanielRamos <yanielramos0504@gmail.com>
2023-06-15 20:45:23 -0400

    adding file 4

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -52,10 +52,10 @@ class Rectangle:
         """function that print rectangle or a empty string"""
         if self.__width == 0 or self.__height == 0:
             return ""
-        rectangle_str = ""
+        string = ""
         for _ in range(self.__height):
-            rectangle_str += "#" * self.__width + "\n"
-        return rectangle_str.rstrip()
+            string += "#" * self.__width + "\n"
+        return string.rstrip()
 
     def __repr__(self):
         return f"Rectangle({self.__width}, {self.__height})"
