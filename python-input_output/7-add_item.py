93878eeccaa52050b6ca1e9b3ac154459268900c
YanielRamos <yanielramos0504@gmail.com>
2023-06-21 16:29:39 -0400

    adding file 7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Module that write a script that adds all arguments to a
+Python list and then save them to a file"""
+
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+try:
+    json_list = load_from_json_file("add_item.json")
+except Exception:
+    json_list = []
+
+for argm in sys.argv[1:]:
+    json_list.append(argm)
+save_to_json_file(json_list, "add_item.json")
