79703636a54f7de887f301d0c6dac90eb62b2c0d
YanielRamos <yanielramos0504@gmail.com>
2023-06-21 14:52:36 -0400

    file 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ def read_file(filename=""):
     """function that read and print"""
     with open(filename, "r") as file:
         content = file.read()
-        print(content)
+        print(content, end="")
