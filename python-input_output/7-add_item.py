a3b187a3b9013d4f2c350b0a841a0782cd4bd2be
YanielRamos <yanielramos0504@gmail.com>
2023-06-21 16:41:03 -0400

    file 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,10 +9,10 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
 try:
-    js_list = load_from_json_file("add_item.json")
-except Exception:
-    js_list = []
+    existing_list = load_from_json_file('add_item.json')
+except FileNotFoundError:
+    existing_list = []
 
 for arg in sys.argv[1:]:
-    js_list.append(arg)
-save_to_json_file(js_list, "add_item.json")
+    existing_list.append(arg)
+save_to_json_file(existing_list, "add_item.json")
