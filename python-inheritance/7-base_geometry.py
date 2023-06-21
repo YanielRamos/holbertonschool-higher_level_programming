9720e00d51ec26228b8f8a8fde629d39ccea24fe
YanielRamos <yanielramos0504@gmail.com>
2023-06-21 11:20:44 -0400

    file 7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Module with one class and two public methods"""
+
+
+class BaseGeometry:
+    """Class that have the method"""
+
+    def area(self):
+        """method that raise en message error"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
