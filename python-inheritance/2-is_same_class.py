3b51fa30ddf797490a240319b855060137eb64b2
YanielRamos <yanielramos0504@gmail.com>
2023-06-21 10:32:48 -0400

    file 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""function that returns True if object is exactly
+an instance of the specified class"""
+
+
+def is_same_class(obj, a_class):
+    """function that compares"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
