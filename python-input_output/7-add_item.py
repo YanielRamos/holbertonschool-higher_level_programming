111c1dc928f035d8871f24c4512b1a64c11d8947
YanielRamos <yanielramos0504@gmail.com>
2023-06-21 16:38:03 -0400

    file 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,16 +4,15 @@
 Python list and then save them to a file"""
 import sys
 
-
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
 try:
-    json_list = load_from_json_file("add_item.json")
+    js_list = load_from_json_file("add_item.json")
 except Exception:
-    json_list = []
+    js_list = []
 
 for arg in sys.argv[1:]:
-    json_list.append(arg)
-save_to_json_file(json_list, "add_item.json")
+    js_list.append(arg)
+save_to_json_file(js_list, "add_item.json")
