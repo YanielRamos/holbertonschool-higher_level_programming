eaef2d56242d600b473c9a4dee0c8c402660884e
YanielRamos <yanielramos0504@gmail.com>
2023-06-21 15:16:39 -0400

    adding file 2

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""module have a function that appends a string at the
+end of a text file"""
+
+
+def append_write(filename="", text=""):
+    with open(filename, "a") as file:
+        return file.append(text)
