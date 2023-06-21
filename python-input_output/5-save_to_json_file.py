d47aa4c363340440fe04f7799c30ed8c15b876bd
YanielRamos <yanielramos0504@gmail.com>
2023-06-21 15:51:03 -0400

    adding file 5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,6 +6,7 @@ import json
 
 
 def save_to_jsoin_file(my_obj, filename):
-    """function that writes a json object the file"""
+    """function that writes a json object to the file"""
+
     with open(filename, "w") as file:
         json.dump(my_obj, file)
