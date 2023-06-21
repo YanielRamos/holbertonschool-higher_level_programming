2bd42331dbdf43d8c278d1d1e1871958cc799cb3
YanielRamos <yanielramos0504@gmail.com>
2023-06-21 15:06:47 -0400

    adding file 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Module of how to write a string to a text file and return
+amount of characters written"""
+
+
+def write_file(filename="", text=""):
+    """function that write and counts"""
+    with open(filename, "w") as file:
+        file.write(text)
+
+    for i in len(file):
+        i += 1
+
+    return i
