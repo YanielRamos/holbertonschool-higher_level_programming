ed3edf47a031f156376d09fcd38d89fef41a1d1b
YanielRamos <yanielramos0504@gmail.com>
2023-06-25 22:36:10 -0400

    adding file 3

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -20,3 +20,50 @@ class Rectangle(Base):
     def width(self):
         """width getter"""
         return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        if not isinstance(value, int):
+            raise TypeError(f"{self.id} must be an integer")
+        elif value < 0:
+            raise ValueError(f"{self.id} must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        if not isinstance(value, int):
+            raise TypeError(f"{self.id} must be an integer")
+        elif value < 0:
+            raise ValueError(f"{self.id} must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """x getter"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """x setter"""
+        if value < 0:
+            raise ValueError(f"{self.id} must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """y getter"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """y setter"""
+        if value < 0:
+            raise ValueError(f"{self.id} must be >= 0")
+        self.__y = value
