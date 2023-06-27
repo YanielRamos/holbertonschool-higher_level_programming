34b800015d7068cb2e4e4959831709ab3d0bc622
YanielRamos <yanielramos0504@gmail.com>
2023-06-27 00:17:20 -0400

    adding dictionary

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -125,3 +125,14 @@ class Rectangle(Base):
         elif kwargs:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """dictionary method"""
+        dictionary = {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
+        return dictionary
