6772f7fa17c90c8d0bb6dc579ed028392a5bfc68
YanielRamos <yanielramos0504@gmail.com>
2023-07-25 20:25:49 -0400

    updating 7

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 --  inserts a new row in the table first_table
 
-INSERT INTO first_table (id, name)
-VALUES (89, 'Best school');
+INSERT INTO 'first_table' ('id', 'name')
+VALUES ('89', 'Best school');
