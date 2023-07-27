a9a01820ca197fe4dc818cbf9914b25127336078
YanielRamos <yanielramos0504@gmail.com>
2023-07-26 20:45:32 -0400

    FIXING 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,7 +1,4 @@
 -- Script that creates the MySQL server user user_0d_1
-
---CREATING USER
+--CREATING USER AND GRANTING PRIVILEGES
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-
--- GRANTING PRIVILEGES
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';;
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
