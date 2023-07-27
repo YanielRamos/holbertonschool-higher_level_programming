55e6cbe1cb2a69a26675e987d5c5b85ca5840c40
YanielRamos <yanielramos0504@gmail.com>
2023-07-26 20:37:51 -0400

    FIXING 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,7 +1,5 @@
 -- Script that creates the MySQL server user user_0d_1
 
--- creating user if not exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- grant all privilages
 GRANT ALL PRIVILAGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILAGES;
