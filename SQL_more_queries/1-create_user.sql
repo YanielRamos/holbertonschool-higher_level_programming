6f7f32ce285543f0488f7e07dde8170bae5980e3
YanielRamos <yanielramos0504@gmail.com>
2023-07-26 20:34:26 -0400

    GRANTING PRIVILAGES

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,10 @@
 -- Script that creates the MySQL server user user_0d_1
 
+-- creating user if not exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- grant all privilages
+GRANT ALL PRIVILAGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- reload privilages
+FLUSH PRIVILAGES;
