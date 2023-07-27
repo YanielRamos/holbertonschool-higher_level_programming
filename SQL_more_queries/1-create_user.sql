d353309655ac7b69523d50ef57607144726103bc
YanielRamos <yanielramos0504@gmail.com>
2023-07-26 20:36:59 -0400

    fixing 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -5,6 +5,3 @@ CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- grant all privilages
 GRANT ALL PRIVILAGES ON *.* TO 'user_0d_1'@'localhost';
-
--- reload privilages
-FLUSH PRIVILAGES;
