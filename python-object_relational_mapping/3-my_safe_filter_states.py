a09781f2c34143d9cd45eb4508bf078f347bee43
YanielRamos <yanielramos0504@gmail.com>
2023-07-31 15:20:23 -0400

    file 3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Lists all values in the states tables of a database where name
-matches the argument"""
+matches the argument in a safe way"""
 import sys
 import MySQLdb
 
@@ -10,13 +10,12 @@ if __name__ == '__main__':
         passwd=sys.argv[2],
         db=sys.argv[3],
         host='localhost',
-        port=3306
+        port=3306,
     )
+    state_name = sys.argv[4]
 
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states
-                WHERE name LIKE BINARY '{}'
-                ORDER BY id ASC""".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s", state_name)
     states = cur.fetchall()
 
     for state in states:
