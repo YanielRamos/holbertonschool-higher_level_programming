4320f7f3a406098fa4b80a937784731436b958ed
YanielRamos <yanielramos0504@gmail.com>
2023-07-31 16:21:55 -0400

    file 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""python file that contains the class definition of a State"""
+"""
+Contains State class and Base, an instance of declarative_base()
+"""
 
 import sqlalchemy
 from sqlalchemy import Column, Integer, String, ForeignKey
@@ -7,7 +9,7 @@ from model_state import Base, State
 
 
 class City(Base):
-    """Class that represents a city"""
+    """Class with id and name attributes of each state"""
     _tablename__ = "cities"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
