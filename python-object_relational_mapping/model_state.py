32e15fcc25caf542e1c2d1840b557b88d31e86f7
YanielRamos <yanielramos0504@gmail.com>
2023-07-31 16:26:10 -0400

    fixing

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -2,15 +2,16 @@
 """
 Contains State class and Base, an instance of declarative_base()
 """
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-import sqlalchemy
-from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base, State
+Base = declarative_base()
 
 
-class City(Base):
-    """Class with id and name attributes of each state"""
-    _tablename__ = "cities"
-    id = Column(Integer, primary_key=True)
+class State(Base):
+    """
+    Class with id and name attributes of each state
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('state_id'))
